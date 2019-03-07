dcf3cde1016f7c886a8ce5a5c3052371624b22bb
Carlo Peroni <carloperoni.italy@gmail.com>
2019-03-07 10:27:30 +0100

    layout definitivo area riservata

--- src/app/area-riservata/area-riservata.component.ts
+++ src/app/area-riservata/area-riservata.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-area-riservata',
   templateUrl: './area-riservata.component.html',
-  styleUrls: ['./area-riservata.component.css']
+  styleUrls: ['./area-riservata.component.css',
+  '../vendor/bootstrap/css/bootstrap.min.css',
+  '../css/simple-sidebar.css' ]
 })
 export class AreaRiservataComponent implements OnInit {
 
