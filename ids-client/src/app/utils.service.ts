7ffcddd18ab67d33eac3802d95f46957d658b307
SaraGiambe <saragiamberini@gmail.com>
2019-04-08 13:15:35 +0200

    cambiato nome a app.js in webService.js

--- ids-client/src/app/utils.service.ts
+++ ids-client/src/app/utils.service.ts
@@ -8,26 +8,6 @@ export class UtilsService {
 
   constructor(private http: HttpClient) { }
 
-
-  isAuthenticated(username, password) {
-    let test = false;
-    if (((username === 'direttore') || (username === 'rup') || (username === 'ditta')) && (password === '123')) {
-
-      const user =  {
-        username: username,
-        password: password
-      };
-      console.log(JSON.stringify(user) + ' has been authenticated!');
-      localStorage.setItem('user', user.username);
-      test = true;
-
-    } else {
-      test = false;
-    }
-
-    return test;
-  }
-
   validateLogin(username, password) {
     console.log(username + ' ' + password);
     return this.http.post('/api/user/login', {
