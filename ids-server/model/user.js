2936b1ce830a672e49bd64d031e6c9256038a113
Morris Capparuccini <morris.capparuccini@gmail.com>
2019-05-09 12:09:36 +0200

    modifica schema user

--- ids-server/model/user.js
+++ ids-server/model/user.js
@@ -6,6 +6,14 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   title: { type: String, required: true },
+  data: { type: Date, required: true },
+  codiceFiscale: { type: String, required: true },
+  residenza: { type: String, required: true },
+  email: { type: String, required: true },
+  telefono: { type: Number, required: true, validate: { validator: Number.isInteger, message : '{VALUE} non è intero'} },
+  citta: { type: String, required: true },
+  provincia: { type: String, required: true },
+  CAP: { type: Number, required: true, validate: { validator: Number.isInteger, message : '{VALUE} non è intero'} },
   keystore: { type: Object, required: true },
   contracts : { type: [{ type: Schema.Types.ObjectId, ref:'Contract' }] }
 }, { collection : 'Users' });
