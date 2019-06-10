6981445ff77f515c8688898e2405235bb6b24a01
mattialuzi <mattialuzi@gmail.com>
2019-06-10 20:16:28 +0200

    aggiornati schemi mongoose

--- ids-server/model/contract.js
+++ ids-server/model/contract.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
 // create a schema
-const moduleSchema = new Schema({
+const smartContractSchema = new Schema({
   address: { type: String, required: true }
 })
 
-const modulesSchema = new Schema({
-  giornaleLavori: { type: moduleSchema, required: true },
-  librettoMisure: { type: moduleSchema, required: true },
-  registroContabile: { type: moduleSchema, required: true },
-  sal: { type: moduleSchema, required: true }
-})
+// const modulesSchema = new Schema({
+//   giornaleLavori: { type: moduleSchema, required: true },
+//   librettoMisure: { type: moduleSchema, required: true },
+//   registroContabile: { type: moduleSchema, required: true },
+//   sal: { type: moduleSchema, required: true }
+// })
 
 const contractSchema = new Schema({
-  name: { type: String, required: true},
-  address: { type: String, required: true },
-  modules: { type: modulesSchema, required: true},
+  nome: { type: String, required: true},
+  libretto: { type: smartContractSchema, required: true},
+  giornale: { type: smartContractSchema, required: true},
+  registro: { type: smartContractSchema, required: true},
+  parametri: { type: smartContractSchema, required: true},
+  sal: { type: smartContractSchema, required: true}
+  // address: { type: String, required: true },
+  // modules: { type: modulesSchema, required: true},
 }, { collection : 'Contracts' });
 
 const contract = mongoose.model('Contract', contractSchema);
