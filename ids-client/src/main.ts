95419a5c1f7e9087f36d88fa7b2ba11bbad612c8
mattialuzi <mattialuzi@gmail.com>
2019-06-17 00:26:58 +0200

    console.log suppress on production

--- ids-client/src/main.ts
+++ ids-client/src/main.ts
@@ -8,7 +8,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  window.console.log = function () { };   // disable any console.log debugging statements in production mode
+  // window.console.error = function () { };
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
