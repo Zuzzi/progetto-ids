444a3183589a510f226f0307c9e67eaa7243b791
SaraGiambe <saragiamberini@gmail.com>
2019-09-03 12:09:31 +0200

    modifiche dialog info contratto

--- ids-client/src/app/components/dialog-body-infocontratto/dialog-body-infocontratto.component.ts
+++ ids-client/src/app/components/dialog-body-infocontratto/dialog-body-infocontratto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DialogInfoContratto } from '@app/interfaces';
+import { ParametriService } from '@app/services/parametri/parametri.service';
+import { filter, switchMap, map, tap, delay, share, shareReplay, concatMapTo, publishReplay, refCount, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dialog-body-infocontratto',
@@ -8,13 +10,23 @@ import { DialogInfoContratto } from '@app/interfaces';
   styleUrls: ['./dialog-body-infocontratto.component.css']
 })
 export class DialogBodyInfocontrattoComponent implements OnInit {
-
+  valoreTotaleSource;
   constructor(public dialogRef: MatDialogRef<DialogBodyInfocontrattoComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: DialogInfoContratto) { }
+              @Inject(MAT_DIALOG_DATA) public data: DialogInfoContratto,
+              private parametriService: ParametriService) { }
+
 
   ngOnInit() {
+    this.valoreTotaleSource = this.parametriService.valoretotale.pipe(
+      tap(value => console.log(value)),
+      publishReplay(1),
+      refCount()
+    );
   }
 
+  conversione(valore) {
+    return valore / (Math.pow(2, 64));
+  }
 
   close(): void {
     this.dialogRef.close();
