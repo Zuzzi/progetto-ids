d3393d3a8af9a97e79e6fa84431ffb674bca1129
mattialuzi <mattialuzi@gmail.com>
2019-09-13 08:45:01 +0200

    edit byzantium start

--- byzantium-start.sh
+++ byzantium-start.sh
@@ -66,7 +66,7 @@ fi
 
 echo "[*] Starting Ethereum nodes with ChainID and NetworkId of $NETWORK_ID"
 set -v
-ARGS="--nodiscover --istanbul.blockperiod 5 --networkid $NETWORK_ID --syncmode full --mine --minerthreads 1 --ws --wsaddr 0.0.0.0 --wsorigins="*" --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
+ARGS="--nodiscover --istanbul.blockperiod 5 --networkid $NETWORK_ID --syncmode full --mine --minerthreads 1 --ws --wsaddr 0.0.0.0 --wsorigins=\"*\" --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
 PRIVATE_CONFIG=qdata/c1/tm.ipc nohup geth --datadir qdata/dd1 $ARGS --wsport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/1.log &
 PRIVATE_CONFIG=qdata/c2/tm.ipc nohup geth --datadir qdata/dd2 $ARGS --wsport 22001 --port 21001 --unlock 0 --password passwords.txt 2>>qdata/logs/2.log &
 PRIVATE_CONFIG=qdata/c3/tm.ipc nohup geth --datadir qdata/dd3 $ARGS --wsport 22002 --port 21002 --unlock 0 --password passwords.txt 2>>qdata/logs/3.log &
